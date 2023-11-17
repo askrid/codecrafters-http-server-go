106a5891dfc3f3602f306eb4d70445a047b31a92
askrid <joonwoo3023@gmail.com>
2023-11-18 03:11:10 +0900

    fix

--- app/server.go
+++ app/server.go
@@ -7,11 +7,8 @@ import (
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	Uncomment this block to pass the first stage
-	
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
@@ -24,3 +21,4 @@ func main() {
 		os.Exit(1)
 	}
 }
+
