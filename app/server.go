8e8b439548d24ef3d33041fddc85f2ed6014efd0
askrid <joonwoo3023@gmail.com>
2023-11-18 21:39:53 +0900

    return 200

--- app/server.go
+++ app/server.go
@@ -7,18 +7,26 @@ import (
 )
 
 func main() {
-	fmt.Println("Logs from your program will appear here!")
-
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	
-	_, err = l.Accept()
+
+	fmt.Println("Started the server")
+
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-}
 
+	header := "HTTP/1.1 200 OK\r\n\r\n"
+	_, err = conn.Write([]byte(header))
+	if err != nil {
+		fmt.Println("Error writing message: ", err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Println("Response sent successfully")
+}
